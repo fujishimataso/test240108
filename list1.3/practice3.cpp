6ef270fd68a398c1744b603c4d57f5e1d6516646
Yuya Hirata <110040340+fujishimataso@users.noreply.github.com>
2024-01-08 19:31:43 +0900

    Create practice3.cpp

---
+++ list1.3/practice3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+void show_messages()
+{
+    std::cout<<"hello,world"<<std::endl;
+    // return;
+    std::cout<<"hello,return"<<std::endl;
+}
+
+int main()
+{
+    show_messages();
+}
