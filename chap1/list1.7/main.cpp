c5704b15f32e5a25e5100327dbd6dc4930a17995
Yuya Hirata <110040340+fujishimataso@users.noreply.github.com>
2024-01-08 19:50:21 +0900

    Create main.cpp

---
+++ chap1/list1.7/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int a = 10;
+    if (10 < a) // 変数aと10を比較
+    {
+        
+    }
+    else
+    {
+        std::cout << "変数aは10以下です" << std::endl;
+    }
+    std::cout << "if文のあと" << std::endl;
+}
